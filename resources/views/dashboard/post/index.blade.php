cc878f8b53c6cf94c780f531e4ec932f14ef5582
Andres Justiniano <andres95liverpool@gmail.com>
2021-08-25 20:11:49 -0400

    finalización del crud post

--- resources/views/dashboard/post/index.blade.php
+++ resources/views/dashboard/post/index.blade.php
@@ -30,9 +30,9 @@
                     <a href="{{ route('post.show',$post->id) }}" class="btn btn-primary">Ver</a>
                     <a href="{{ route('post.edit',$post->id) }}" class="btn btn-primary">Editar</a>
                     
-                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal" data-id="{{ $post->id }}">
-                            Eliminar
-                        </button>
+                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal" data-id="{{ $post->id }}">
+                        Eliminar
+                    </button>
                     
                 </td>
             </tr>
@@ -45,41 +45,47 @@
         <div class="modal-dialog">
             <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+                <h5 class="modal-title" id="modalLabel"></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <form>
-                <div class="form-group">
-                    <label for="recipient-name" class="col-form-label">Recipient:</label>
-                    <input type="text" class="form-control" id="recipient-name">
-                </div>
-                <div class="form-group">
-                    <label for="message-text" class="col-form-label">Message:</label>
-                    <textarea class="form-control" id="message-text"></textarea>
-                </div>
-                </form>
+                <p>Seguro que deseas eliminar el post seleccionado?</p>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Send message</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+
+                <form id="formDelete" action="{{ route('post.destroy',0) }}" method="post" data-action="{{ route('post.destroy',0) }}" >
+                    @method('DELETE')
+                    @csrf
+                    <button type="submit" class="btn btn-danger">Borrar</button>
+                </form>
+
             </div>
             </div>
         </div>
     </div>
 
     <script>
-        $('#deleteModal').on('show.bs.modal', function (event) {
-            var button = $(event.relatedTarget) // Button that triggered the modal
-            var recipient = button.data('whatever') // Extract info from data-* attributes
-            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-            var modal = $(this)
-            modal.find('.modal-title').text('New message to ' + recipient)
-            modal.find('.modal-body input').val(recipient)
-        })
+        window.onload = function () {
+            $('#deleteModal').on('show.bs.modal', function (event) {
+
+                var button = $(event.relatedTarget) // Button that triggered the modal
+                var id = button.data('id') // Extract info from data-* attributes
+                // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+                // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+
+                action = $('#formDelete').attr('data-action').slice(0,-1)
+
+                $('#formDelete').attr('action',action + id )
+
+                var modal = $(this)
+                modal.find('.modal-title').text('Vas a borrar el POST ' + id)
+                
+            });
+        };
+
 
     </script>
 
