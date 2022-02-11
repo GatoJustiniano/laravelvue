e0a50158413ec5183f8601d43b2bf521a8a73e46
Andres Justiniano <andres95liverpool@gmail.com>
2022-02-10 23:13:27 -0400

    fix

--- app/Http/Controllers/dashboard/PostController.php
+++ app/Http/Controllers/dashboard/PostController.php
@@ -27,7 +27,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::orderBy('created_at','asc')->paginate(5);
+        $posts = Post::orderBy('created_at','asc')->paginate(6);
         return view('dashboard/post/index',['posts' => $posts]);
 
     }
