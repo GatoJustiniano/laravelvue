e0297383490a772371f699a4fa1e1cef8e089257
GatoJustiniano <andres95liverpool@gmail.com>
2024-02-23 23:51:39 -0400

    Product reparar error

--- app/Http/Controllers/dashboard/ProductController.php
+++ app/Http/Controllers/dashboard/ProductController.php
@@ -2,9 +2,84 @@
 
 namespace App\Http\Controllers\dashboard;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('products.index')->with([
+            'products' => Product::without('images')->get(),
+        ]);
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(ProductRequest $request)
+    {
+        $product = Product::create($request->validated());
+
+        foreach ($request->images as $image) {
+            $product->images()->create([
+                'path' => 'images/' . $image->store('products', 'images'),
+            ]);
+        }
+
+        return redirect()
+            ->route('products.index')
+            ->withSuccess("The new product with id {$product->id} was created");
+        // ->with(['success' => "The new product with id {$product->id} was created"]);
+    }
+
+    public function show(Product $product)
+    {
+        return view('products.show')->with([
+            'product' => $product,
+        ]);
+    }
+
+    public function edit(Product $product)
+    {
+        return view('products.edit')->with([
+            'product' => $product,
+        ]);
+    }
+
+    public function update(ProductRequest $request, Product $product)
+    {
+        $product->update($request->validated());
+
+        if ($request->hasFile('images')) {
+            foreach ($product->images as $image) {
+                $path = storage_path("app/public/{$image->path}");
+
+                File::delete($path);
+
+                $image->delete();
+            }
+
+            foreach ($request->images as $image) {
+                $product->images()->create([
+                    'path' => 'images/' . $image->store('products', 'images'),
+                ]);
+            }
+        }
+
+        return redirect()
+            ->route('products.index')
+            ->withSuccess("The product with id {$product->id} was edited");
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return redirect()
+            ->route('products.index')
+            ->withSuccess("The product with id {$product->id} was deleted");
+    }
 }
