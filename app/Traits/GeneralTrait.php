21ef0af78cd00194e982e518f67ceba279f6c898
GatoJustiniano <andres95liverpool@gmail.com>
2024-03-16 22:39:39 -0400

    update

--- app/Traits/GeneralTrait.php
+++ app/Traits/GeneralTrait.php
@@ -7,15 +7,22 @@ use Illuminate\Support\Facades\Gate;
 trait GeneralTrait {
 
     function generateButtons($name, $id) {
-
         $buttonsHtml = '';
 
         if (Gate::allows("${name}.show")) {
-            $buttonsHtml .= '<a href="' . route("${name}.show", $id) . '" class="btn btn-outline-secondary btn-sm btn-icon"> <i class="material-icons">visibility</i> </a>';
+            $buttonsHtml .= '
+                <a href="' . route("${name}.show", $id) . '" 
+                    class="btn btn-outline-secondary btn-sm btn-icon"> 
+                    <i class="material-icons">visibility</i> 
+                </a>';
         }
     
         if (Gate::allows("${name}.edit")) {
-            $buttonsHtml .= '<a href="' . route("${name}.edit", $id) . '" class="btn btn-outline-secondary btn-sm btn-icon"> <i class="material-icons">edit</i> </a>';
+            $buttonsHtml .= '
+                <a href="' . route("${name}.edit", $id) . '" 
+                    class="btn btn-outline-secondary btn-sm btn-icon">
+                    <i class="material-icons">edit</i>
+                </a>';
         }              
         return $buttonsHtml;
     }
